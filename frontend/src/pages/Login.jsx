d6ffed105f9e11c3f966c34fdd16b07259e20f07
BEN RICH <benrich205@gmail.com>
2025-08-06 16:19:47 +0300

    add email in Login Page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,7 +20,8 @@ const Login = () => {
       <div className="min-h-screen bg-cover bg-center flex items-center justify-center"
         style={{ backgroundImage: "url('./src/assets/background.jpg')" }}>
         <div>
-          <form className="backdrop-blur-xl space-y-6 border border-black/10 p-10 rounded-xl text-white text-center shadow-lg max-w-xl">
+          <form
+              className="backdrop-blur-xl space-y-6 border border-black/10 p-10 rounded-xl text-white text-center shadow-lg max-w-xl">
 
             <h3 className="text-2xl font-bold mb-4 drop-shadow text-white text-center">
               Employee Management System
@@ -29,24 +30,32 @@ const Login = () => {
             <h3 className='font-bold text-lg'>LOGIN</h3>
 
             <input
-              id='username'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder='Username'
-              className='backdrop-blur-2xl border border-b-amber-50-white/10 p-2 rounded-lg text-amber-50 shadow-lg'
+                id='username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder='email'
+                className='backdrop-blur-2xl border border-b-amber-50-white/10 p-2 rounded-lg text-amber-50 shadow-lg'
             />
 
             <input
-              id='Password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder='Password'
-              className='backdrop-blur-2xl border border-b-amber-50-white/10 p-2 rounded-xl text-white shadow-lg'
+                id='username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder='Username'
+                className='backdrop-blur-2xl border border-b-amber-50-white/10 p-2 rounded-lg text-amber-50 shadow-lg'
+            />
+
+            <input
+                id='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Password'
+                className='backdrop-blur-2xl border border-b-amber-50-white/10 p-2 rounded-xl text-white shadow-lg'
             />
 
             <button
-              className="bg-green-700 px-4 py-1 rounded-lg hover:bg-emerald-950 hover:text-white transition-colors duration-300 shadow-lg flex items-center justify-center mx-auto"
-              onClick={handleSubmit}>
+                className="bg-green-700 px-4 py-1 rounded-lg hover:bg-emerald-950 hover:text-white transition-colors duration-300 shadow-lg flex items-center justify-center mx-auto"
+                onClick={handleSubmit}>
               <span>LOGIN</span>
             </button>
 
