d3e92831cb8734f2cb1e6cfe21daec5354b03bd4
benrich09 <benottovon@gmail.com>
2025-09-01 08:50:51 +0300

    Add of new cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import UserDashboard from './components/UserDashboard';
+import UserDashboard from './components/user/UserDashboard.jsx';
 import AdminDashboard from './components/Admin/AdminDashboard.jsx';
 import Department from "./components/Admin/Department.jsx";
 import LeaveManagement from "./components/Admin/LeaveManagement.jsx";
 import Employees from "./components/Admin/Employees.jsx";
 import Payroll from "./components/Admin/Payroll.jsx";
-import Layout from "components/Admin/Layout.jsx";
-
 function App() {
     return (
         <Router>
@@ -19,11 +17,10 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/dashboard" element={<UserDashboard />} />
                 <Route path="/admin" element={<AdminDashboard />} />
-                <Route path="/admin/employees" element={<Employees/>} />
-                <Route path="/admin/departments" element={<Department/>} />
-                <Route path="/admin/payroll" element={<Payroll/>} />
-                <Route path="/admin/leaves" element={<LeaveManagement/>} />
-                <Route path="/admin/layout" element={<Layout/>} />
+                <Route path="/admin/employees" element={<Employees />} />
+                <Route path="/admin/departments" element={<Department />} />
+                <Route path="/admin/payroll" element={<Payroll />} />
+                <Route path="/admin/leaves" element={<LeaveManagement />} />
             </Routes>
         </Router>
     );
