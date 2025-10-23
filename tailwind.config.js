0546000b6cd8cadd0fb61ab3badf8efdcfeae2c1
benkali <benrich@kali.kaliben>
2025-10-23 08:48:40 -0400

    nimechoka

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
-// tailwind.config.js
-module.exports = {
-    darkMode: 'class',
-    // ...other config
-};
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
